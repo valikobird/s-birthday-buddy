2c37eecbeecab05033972b3cfe85f5c2e57c41db
Valentyn Zozulia <valikobird@protonmail.com>
2025-02-14 18:24:04 +0100

    Implement birthdays reminder list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from 'react';
+import data from './data';
+import List from './components/List';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClearAll = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays Today</h3>
+
+        <List people={people} />
+
+        <button
+          type='button'
+          onClick={handleClearAll}
+          className='btn btn-block'
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import Person from './Person';
+
+const List = ({ people }) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person {...person} key={person.id} />;
+      })}
+    </section>
+  );
+};
+
+export default List;
